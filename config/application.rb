dfda277247a53e986b7822e75569631c4b81c44c
kunjut <v.dimnich@gmail.com>
2019-10-19 20:35:22 +0300

    del option from confi

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ module Blog
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-    config.time_zone = 'Europe/Moscow'
+#    config.time_zone = 'Europe/Moscow' - и без этой опции работает как надо?
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
