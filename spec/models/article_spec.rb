fbf63de4c6d284fc0f1dfef83b1acf117823576c
kunjut <v.dimnich@gmail.com>
2019-10-22 22:17:12 +0300

    rspec test for article model with nested describe

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -2,5 +2,13 @@
 require 'rails_helper'
 
 describe Article do
-  it { should have_many :comments }
+  
+  describe "validations" do
+    it { should validate_presence_of :title }
+    it { should validate_presence_of :content }
+  end
+
+  describe "associations" do
+    it { should have_many :comments }
+  end
 end
