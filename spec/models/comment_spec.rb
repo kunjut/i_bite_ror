ce3be397e48007d55902129ed0d89e90dfcb6f87
kunjut <v.dimnich@gmail.com>
2019-10-24 14:16:35 +0300

    hw: add test for length validations

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 describe Comment do
-  it { should belong_to :article }
+  
+  describe "validations" do
+    it { should validate_length_of(:body).is_at_most(4000) }
+  end
+
+  describe "associations" do
+    it { should belong_to :article }
+  end
 end
