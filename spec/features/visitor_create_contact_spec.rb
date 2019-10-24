8b44d56446fdd257f20d4472401dd873c3c28d9f
kunjut <v.dimnich@gmail.com>
2019-10-24 18:02:31 +0300

    update create contacts with i18n

--- spec/features/visitor_create_contact_spec.rb
+++ spec/features/visitor_create_contact_spec.rb
@@ -3,6 +3,6 @@ require 'rails_helper'
 feature "Contact creation" do
   scenario "allows acess to contacts page" do
     visit '/contacts'
-    expect(page).to have_content 'Contact me'
+    expect(page).to have_content I18n.t 'contacts.contact_me'
   end
 end
