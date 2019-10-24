462680625e9e1c7514325d8ea9573f6f842f4079
kunjut <v.dimnich@gmail.com>
2019-10-24 17:16:43 +0300

    add create contact acceptance tests

---
+++ spec/features/visitor_create_contact_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+feature "Contact creation" do
+  scenario "allows acess to contacts page" do
+    visit '/contacts'
+    expect(page).to have_content 'Contact me'
+  end
+end
