d53e5bf5b656c23750b0fe5e1d84ce01e9b3b58c
kunjut <v.dimnich@gmail.com>
2019-10-26 01:11:59 +0300

    add scenario - allows user to add a new article

--- spec/features/user_create_article_spec.rb
+++ spec/features/user_create_article_spec.rb
@@ -6,9 +6,23 @@ feature "Article creation" do
     sign_up
   end
 
-  #пользователь может попасть на страницу создания статьи?
+  # позволяет пользователю попасть на страницу создания статьи
   scenario "allows user to visit new article page" do
     visit new_article_path
-    expect(page).to have_content 'Add new article'
+    expect(page).to have_content I18n.t('articles.add_new')
+  end
+
+  # позволяет пользователю добавить новую статью
+  scenario "allows user to add a new article" do
+    visit new_article_path
+
+    fill_in :article_title, with: I18n.t('articles.title_stub')
+    fill_in :article_content, with: I18n.t('articles.content_stub')
+
+    click_button I18n.t('articles.btn_save_article')
+
+    expect(page).to have_content I18n.t('articles.title_stub')
+    expect(page).to have_content I18n.t('articles.content_stub')
+  #  expect(page).to have_content I18n.t('current_user.username')
   end
 end
