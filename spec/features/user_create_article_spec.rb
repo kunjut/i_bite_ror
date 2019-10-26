916f325e697a54113658c710cad1bc1c1775aa66
kunjut <v.dimnich@gmail.com>
2019-10-27 00:36:51 +0300

    homework add tests

--- spec/features/user_create_article_spec.rb
+++ spec/features/user_create_article_spec.rb
@@ -4,6 +4,7 @@ feature "Article creation" do
   
   before(:each) do
     sign_up
+    add_article
   end
 
   # позволяет пользователю попасть на страницу создания статьи
@@ -14,15 +15,31 @@ feature "Article creation" do
 
   # позволяет пользователю добавить новую статью
   scenario "allows user to add a new article" do
-    visit new_article_path
-
-    fill_in :article_title, with: I18n.t('articles.title_stub')
-    fill_in :article_content, with: I18n.t('articles.content_stub')
-
-    click_button I18n.t('articles.btn_save_article')
-
     expect(page).to have_content I18n.t('articles.title_stub')
     expect(page).to have_content I18n.t('articles.content_stub')
-  #  expect(page).to have_content I18n.t('current_user.username')
+  end
+
+  # проверка добавления комментария к статье
+  scenario "allows user add comment to article" do 
+    fill_in :comment_body, with: I18n.t('articles.comment')
+    click_button I18n.t('articles.btn_save_comment')
+    expect(page).to have_content I18n.t('articles.comment')
+  end
+
+  # проверка доступа к странице редактирования статьи
+  scenario "allows user visit to article editing page" do 
+    click_on('Edit')
+    expect(page).to have_field(:article_title, with: I18n.t('articles.title_stub'))
+    expect(page).to have_content I18n.t('articles.content_stub')
+  end
+
+  # проверка успешного редактирования статьи
+  scenario "allows user successful article editing" do
+    click_on('Edit')
+    fill_in :article_title, with: I18n.t('articles.title_upd_stub')
+    fill_in :article_content, with: I18n.t('articles.content_upd_stub')
+    click_button I18n.t('articles.btn_upd_article')
+    expect(page).to have_content I18n.t('articles.title_upd_stub')
+    expect(page).to have_content I18n.t('articles.content_upd_stub')  
   end
 end
