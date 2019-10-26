2b8f783488e9eaf9a261d6764f7a470b95aa60b7
kunjut <v.dimnich@gmail.com>
2019-10-27 00:35:57 +0300

    minor changes

--- spec/features/visitor_create_contact_spec.rb
+++ spec/features/visitor_create_contact_spec.rb
@@ -1,11 +1,13 @@
 require 'rails_helper'
 
 feature "Contact creation" do
+  
   #проверка доступа к странице
   scenario "allows access to contacts page" do
     visit '/contacts'
     expect(page).to have_content I18n.t 'contacts.contact_me'
   end
+  
   #проверка возможности создать сообщение в форме контакта
   scenario "allows a guest to create contact" do
     visit '/contacts'
